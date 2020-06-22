1efd56afd494944a1f1fc44d56938c3abb7c2502
Student <student@Makerss-Air-2.broadband>
2020-06-22 15:46:54 +0100

    first commit

---
+++ Fizzbuzz/Fizzbuzz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fizzbuzz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
